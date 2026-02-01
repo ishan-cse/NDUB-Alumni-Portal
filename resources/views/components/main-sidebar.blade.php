58685171c80f3f239373fb380603c2edb38efb98
ishan-cse <ishanndub@gmail.com>
2026-02-01 19:34:40 +0600

    test2

--- resources/views/components/main-sidebar.blade.php
+++ resources/views/components/main-sidebar.blade.php
@@ -39,7 +39,7 @@
                 <a class="nav-link" href="{{ url('dashboard') }}">
                     <i class="nav-icon fas fa-th"></i>
                     <p>
-                        Dashboardd
+                        Dashboard
                     </p>
                 </a>
                 </li>
